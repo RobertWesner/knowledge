7231d18ddd033caf68261b0d987b201d9f403baf
Robert Wesner <robert@wesner.io>
2025-03-14 18:47:54 +0100

    Added Go

--- index.php
+++ index.php
@@ -6,36 +6,33 @@ function dateToYearFloat(string $date): float
 {
     $timestamp = strtotime($date);
 
-    return (float)date('Y', $timestamp)
-        + ((float)date('n', $timestamp) - 1) / 12;
+    return (float)date('Y', $timestamp) + (
+        (float)date('n', $timestamp) - 1
+        + (float)date('d', $timestamp) / 31
+    ) / 12;
 }
 
 $now = (float)date('Y') + ((float)date('n') - 1) / 12;
 $knowledgeCollections = [];
 
 /*
-THE "ROADMAP" (aka. things that might be fun to learn and possibly useful)
-
-Interested in:
-
-    Functional programming without a clear project goal:
-        - Erlang
-        - Elixir
-        - Haskell
-
-    Compiled general purpose:
-        - Go
-        - Zig (the "better" C)?
-
-    Desktop:
-        - Kotlin
-
-    WebAssembly:
-        - Rust (again)
-        - Kotlin (again)
-
-    Personal hell:
-        - COBOL
+ * THE "ROADMAP" (aka. things that might be fun to learn and possibly useful)
+ *
+ *  Interested in:
+ *      Functional programming without a clear project goal:
+ *          - Erlang
+ *          - Elixir
+ *          - Haskell
+ *
+ *  Compiled general purpose:
+ *      - Zig (the "better" C)?
+ *
+ *   WebAssembly:
+ *      - Rust (again)
+ *      - Kotlin (again)
+ *
+ *  Personal hell:
+ *      - COBOL
 */
 $knowledgeCollections[] = (new KnowledgeCollection('programming_language', 'Programming Languages'))
     ->push('Visual Basic .NET', Knowledge::LEVEL_BASIC, [
@@ -48,8 +45,8 @@ $knowledgeCollections[] = (new KnowledgeCollection('programming_language', 'Prog
     ])
     ->push('C++', Knowledge::LEVEL_BEGINNER, [
         [2019, 2019.5],
-        [2020, 2021.5],
-        [dateToYearFloat('2024-10-30')],
+        [2020, 2020.25],
+        [dateToYearFloat('2024-10-30'), dateToYearFloat('2024-11-17')],
     ])
     ->push('Lua', Knowledge::LEVEL_BEGINNER, [
         [2019, 2020.75],
@@ -66,7 +63,7 @@ $knowledgeCollections[] = (new KnowledgeCollection('programming_language', 'Prog
         [2024.8],
     ])
     ->push('Typescript', Knowledge::LEVEL_PROFICIENT, [
-        [2020],
+        [2020, 2023.3],
     ])
     ->push('NASM - x86 Assembly', Knowledge::LEVEL_BASIC, [
         [2020.2, 2020.3],
@@ -86,17 +83,19 @@ $knowledgeCollections[] = (new KnowledgeCollection('programming_language', 'Prog
     ->push('Kotlin', Knowledge::LEVEL_BASIC, [
         [dateToYearFloat('2024-12-30')],
     ])
-;
+    ->push('Go', Knowledge::LEVEL_BASIC, [
+        [dateToYearFloat('2025-03-07')],
+    ]);
 
 /*
-Svelte sounds like fun.
-Might want to brush up on my React.
-*/
+ * Svelte sounds like fun.
+ * Might want to brush up on my React.
+ */
 $knowledgeCollections[] = (new KnowledgeCollection('lib', 'Frameworks, libraries, and everything in between'))
     ->push('Löve 2D', Knowledge::LEVEL_BEGINNER, [
         [2019, 2020],
     ])
-    ->push('Symfony1', Knowledge::LEVEL_EXPERT, [
+    ->push('Symfony1', Knowledge::LEVEL_PROFICIENT, [
         [2019.75, 2023.3],
     ])
     ->push('jQuery', Knowledge::LEVEL_PROFICIENT, [
@@ -108,9 +107,6 @@ $knowledgeCollections[] = (new KnowledgeCollection('lib', 'Frameworks, libraries
     ->push('Symfony', Knowledge::LEVEL_PROFICIENT, [
         [2020.2, 2023.3],
         [2024.8],
-    ])
-    ->push('Infragistics Ignite Grid 2015', Knowledge::LEVEL_PROFICIENT, [
-        [2020.8, 2022.7],
     ])
     ->push('React', Knowledge::LEVEL_BEGINNER, [
         [2021.2, 2021.85],
@@ -135,13 +131,11 @@ $knowledgeCollections[] = (new KnowledgeCollection('lib', 'Frameworks, libraries
         [2024.7, 2024.9],
     ])
     ->push('Laravel', Knowledge::LEVEL_BEGINNER, [
-        [2024.55],
+        [2024.55, 2024.75],
     ])
     ->push('NativePHP', Knowledge::LEVEL_PROFICIENT, [
         [2024.55],
-    ])
-    // RabbitMQ probably will be here soon...
-;
+    ]);
 
 $knowledgeCollections[] = (new KnowledgeCollection('ide', 'IDEs'))
     ->push('Visual Studio 2010 Express', Knowledge::LEVEL_BASIC, [
@@ -151,22 +145,16 @@ $knowledgeCollections[] = (new KnowledgeCollection('ide', 'IDEs'))
         [2018.2, 2018.8],
         [2019.6, 2019.7],
     ])
-    // I know it's just a text editor with syntax highlighting, you do not have to tell me
-    // Cute chameleon though!
-    ->push('Notepad++', Knowledge::LEVEL_BEGINNER, [
-        [2019, 2019.5],
-    ])
     ->push('Code::Blocks', Knowledge::LEVEL_BEGINNER, [
         [2019, 2019.5],
         [2020, 2021.5],
     ])
-    ->push('Visual Studio Code', Knowledge::LEVEL_PROFICIENT, [
+    ->push('Visual Studio Code', Knowledge::LEVEL_BEGINNER, [
         [2019.5],
     ])
     ->push('Jetbrains', Knowledge::LEVEL_EXPERT, [
         [2019.5],
-    ])
-;
+    ]);
 
 $knowledgeCollections[] = (new KnowledgeCollection('tool', 'Tools and other Software'))
     ->push('Gimp', Knowledge::LEVEL_PROFICIENT, [
@@ -182,12 +170,11 @@ $knowledgeCollections[] = (new KnowledgeCollection('tool', 'Tools and other Soft
         [2022.85],
     ])
     ->push('FreeCAD', Knowledge::LEVEL_BEGINNER, [
-        [dateToYearFloat('2023-07-06')],
+        [dateToYearFloat('2023-07-06'), 2024.5],
     ])
     ->push('Gradle', Knowledge::LEVEL_BASIC, [
         [dateToYearFloat('2024-12-30')],
-    ])
-;
+    ]);
 
 $knowledgeCollections[] = (new KnowledgeCollection('os', 'Operating Systems'))
     ->push('Windows', Knowledge::LEVEL_BEGINNER, [
@@ -200,7 +187,7 @@ $knowledgeCollections[] = (new KnowledgeCollection('os', 'Operating Systems'))
         [dateToYearFloat('2024-10-15')],
     ])
     ->push('Linux Mint Cinnamon', Knowledge::LEVEL_PROFICIENT, [
-        [2019.5, dateToYearFloat('2024-12-02')],
+        [2019.5],
     ])
     ->push('Ubuntu Server', Knowledge::LEVEL_PROFICIENT, [
         [2019.6],
@@ -218,12 +205,11 @@ $knowledgeCollections[] = (new KnowledgeCollection('os', 'Operating Systems'))
         [2022.75, 2023.65],
     ])
     ->push('EndeavourOS', Knowledge::LEVEL_PROFICIENT, [
-        [2023.5],
+        [2023.65],
     ])
     ->push('Kubuntu', Knowledge::LEVEL_PROFICIENT, [
         [dateToYearFloat('2024-12-02')],
-    ])
-;
+    ]);
 
 ob_start();
 ?>
@@ -241,7 +227,7 @@ ob_start();
                 This is a collection of my experience with software development and more.
             </p>
             <p>
-                I have been a full-time software developer since late 2019 and spend much of my time outside work honing these skills.
+                I have been a software developer since late 2019 and spend much of my time outside work honing these skills.
             </p>
             <?php foreach ($knowledgeCollections as $collection): ?>
                 <div class="timeline-container">
@@ -263,8 +249,7 @@ ob_start();
                         <?php endforeach; ?>
                     </div>
                 </div>
-                <?php  ?>
-            <?php endforeach;?>
+            <?php endforeach; ?>
         </main>
         <footer>
             Generated on <?= date('Y-m-d') ?>
